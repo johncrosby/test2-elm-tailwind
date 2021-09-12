ae7dc6ed29b6f520ed1ba17037fb5de829693d12
John Crosby <johncrosby36@gmail.com>
2021-09-12 07:57:29 +0100

    trying to make netlify.toml work

---
+++ my-script.js
@@ -0,0 +1,7 @@
+const elmTailwindModules = require("elm-tailwind-modules");
+const tailwindConfig = require("./tailwind.config.js");
+const autoprefixer = require("autoprefixer");
+const postcssImport = require("postcss-import");
+const postcss = require("postcss");
+const tailwindcss = require("tailwindcss");
+const fs = require("fs").promises;
